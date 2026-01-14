973a7e4837ce3aafa0830f814731f9987768be63
nkkbr <fengqi2016@outlook.com>
2026-01-14 23:44:32 +0900

    update ch11

---
+++ ch11/benchmark_2.cu
@@ -0,0 +1,278 @@
+#include <iostream>
+#include <curand.h>
+#include <chrono>
+#include <ctime>
+#include <iomanip>
+#include "scan_kernels.cuh"
+#define BENCHMARK_REPEAT 1000
+
+// 这份文件比较`scan_kernels.cuh`中的12个kernel之间的性能
+// 要完全客观地比较性能，比较难。难在任务的设计上。在本次测试中，我们将总的任务设定为很大的一个float的数组，在BLOCK_SIZE固定为1024的情况下，每个kernel去计算每个block所对应负责的区域的前缀和。
+// 每个kernel计算出来的，不是全局的前缀和，是每个block所对应负责的区域的前缀和。
+// 但每个block所对应负责的区域的大小，又不是一样的。这就造成了，其实有的kernel的实际工作量，多了一点。但这个多，又很难量化
+// 所以，我们这个测试，并不是一个很严格的benchmarking。
+
+const unsigned int TOTAL_N = (1ULL << 28)*3*5;
+
+void initGaussian(float* d_input, size_t n, float mean, float stddev) {
+    curandGenerator_t gen;
+    curandCreateGenerator(&gen, CURAND_RNG_PSEUDO_DEFAULT);
+    curandSetPseudoRandomGeneratorSeed(gen, 1234ULL);
+    curandGenerateNormal(gen, d_input, n, mean, stddev);
+    curandDestroyGenerator(gen);
+}
+
+int main(){
+
+    auto now = std::chrono::system_clock::now();
+    std::time_t now_time = std::chrono::system_clock::to_time_t(now);
+    std::cout << "Benchmark started at: " 
+            << std::put_time(std::localtime(&now_time), "%Y-%m-%d %H:%M:%S") 
+            << std::endl;
+
+    int dev=0;
+    cudaDeviceProp deviceProp;
+    cudaGetDeviceProperties(&deviceProp, dev);
+    printf("Currently using CUDA device %d: %s\n", dev, deviceProp.name);
+    cudaSetDevice(dev);
+    printf("Total Global Memory: %.2f GB\n", deviceProp.totalGlobalMem / (1024.0*1024.0*1024.0));
+    printf("Benchmark Data Size: %u elements (%.2f GB per array)\n", TOTAL_N, (double)TOTAL_N * 4 / (1024.0*1024.0*1024.0));
+    printf("------------------------------------------------------------------------------------------------------------\n");
+
+    float *d_input, *d_output;
+    cudaMalloc(&d_input, (size_t)TOTAL_N*sizeof(float));
+    cudaMalloc(&d_output, (size_t)TOTAL_N*sizeof(float));
+
+    initGaussian(d_input, TOTAL_N, 0.0f, 1.0f);
+
+    int items = 1024;
+    int grid_size = (TOTAL_N+items-1)/items;
+
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+    float milliseconds;
+
+    dim3 block(1);
+    dim3 grid(1);
+
+    double total_bytes = (double)TOTAL_N * sizeof(float) * 2; 
+    double gb_per_sec;
+
+    unsigned int block_sizes[] = {64, 128, 256, 512, 1024};
+    for (unsigned int BLOCK_SIZE : block_sizes) {
+        printf("\n\n################################################################\n");
+        printf("Starting Benchmark for Block Size: %5d\n", BLOCK_SIZE);
+        printf("################################################################\n\n\n");
+        //////////////////////////////////////////////////////////////////////////////////////
+        //     1. Kogge_Stone_scan_kernel_shfl_up_sync_version_coarsening_degree_4
+        //////////////////////////////////////////////////////////////////////////////////////
+        items = BLOCK_SIZE*4;
+        grid_size = (TOTAL_N+items-1)/items;
+
+        block.x = BLOCK_SIZE;
+        grid.x = grid_size;
+        printf("grid:  %5d, %5d, %5d\n", grid.x, grid.y, grid.z);
+        printf("block: %5d, %5d, %5d\n", block.x, block.y, block.z);
+
+        cudaError_t err;
+
+        // 2.1 Warmup
+        for (int i=0;i<10;i++){
+            Kogge_Stone_scan_kernel_shfl_up_sync_version_coarsening_degree_4<<<grid, block>>>(d_input, d_output, TOTAL_N);
+        }
+        cudaDeviceSynchronize();
+        printf("Warmup complete for Kogge_Stone_scan_kernel_shfl_up_sync_version_coarsening_degree_4\n");
+
+        // 2.2 Benchmarking
+        cudaEventRecord(start);
+        for (int i=0;i<BENCHMARK_REPEAT;i++){
+            Kogge_Stone_scan_kernel_shfl_up_sync_version_coarsening_degree_4<<<grid, block>>>(d_input, d_output, TOTAL_N);
+        }
+        cudaEventRecord(stop);
+        cudaEventSynchronize(stop);
+
+        // 防止静默出错（但这一部分似乎也是不必要的）
+        err = cudaGetLastError();
+        if (err != cudaSuccess) {
+            printf("!! Kernel Launch Failed !! Reason: %s\n", cudaGetErrorString(err));
+        }
+
+        cudaEventElapsedTime(&milliseconds, start, stop);
+        printf("Execution time for Kogge_Stone_scan_kernel_shfl_up_sync_version_coarsening_degree_4: %f ms\n\n\n",milliseconds/BENCHMARK_REPEAT);
+        gb_per_sec = total_bytes / ((milliseconds / BENCHMARK_REPEAT) / 1000.0) / 1e9;
+        printf("Effective Bandwidth: %.2f GB/s\n", gb_per_sec);
+        printf("------------------------------------------------------------------------------------------------------------\n");
+
+
+        //////////////////////////////////////////////////////////////////////////////////////
+        //     2. Kogge_Stone_scan_kernel_shfl_up_sync_version_coarsening_degree_8
+        //////////////////////////////////////////////////////////////////////////////////////
+        items = BLOCK_SIZE*8;
+        grid_size = (TOTAL_N+items-1)/items;
+
+        block.x = BLOCK_SIZE;
+        grid.x = grid_size;
+        printf("grid:  %5d, %5d, %5d\n", grid.x, grid.y, grid.z);
+        printf("block: %5d, %5d, %5d\n", block.x, block.y, block.z);
+
+        // 2.1 Warmup
+        for (int i=0;i<10;i++){
+            Kogge_Stone_scan_kernel_shfl_up_sync_version_coarsening_degree_8<<<grid, block>>>(d_input, d_output, TOTAL_N);
+        }
+        cudaDeviceSynchronize();
+        printf("Warmup complete for Kogge_Stone_scan_kernel_shfl_up_sync_version_coarsening_degree_8\n");
+
+        // 2.2 Benchmarking
+        cudaEventRecord(start);
+        for (int i=0;i<BENCHMARK_REPEAT;i++){
+            Kogge_Stone_scan_kernel_shfl_up_sync_version_coarsening_degree_8<<<grid, block>>>(d_input, d_output, TOTAL_N);
+        }
+        cudaEventRecord(stop);
+        cudaEventSynchronize(stop);
+
+        // 防止静默出错（但这一部分似乎也是不必要的）
+        err = cudaGetLastError();
+        if (err != cudaSuccess) {
+            printf("!! Kernel Launch Failed !! Reason: %s\n", cudaGetErrorString(err));
+        }
+
+        cudaEventElapsedTime(&milliseconds, start, stop);
+        printf("Execution time for Kogge_Stone_scan_kernel_shfl_up_sync_version_coarsening_degree_8: %f ms\n\n\n",milliseconds/BENCHMARK_REPEAT);
+        gb_per_sec = total_bytes / ((milliseconds / BENCHMARK_REPEAT) / 1000.0) / 1e9;
+        printf("Effective Bandwidth: %.2f GB/s\n", gb_per_sec);
+        printf("------------------------------------------------------------------------------------------------------------\n");
+
+
+        //////////////////////////////////////////////////////////////////////////////////////
+        //     3. Kogge_Stone_scan_kernel_shfl_up_sync_version_coarsening_degree_12
+        //////////////////////////////////////////////////////////////////////////////////////
+        items = BLOCK_SIZE*12;
+        grid_size = (TOTAL_N+items-1)/items;
+
+        block.x = BLOCK_SIZE;
+        grid.x = grid_size;
+        printf("grid:  %5d, %5d, %5d\n", grid.x, grid.y, grid.z);
+        printf("block: %5d, %5d, %5d\n", block.x, block.y, block.z);
+
+        // 2.1 Warmup
+        for (int i=0;i<10;i++){
+            Kogge_Stone_scan_kernel_shfl_up_sync_version_coarsening_degree_12<<<grid, block>>>(d_input, d_output, TOTAL_N);
+        }
+        cudaDeviceSynchronize();
+        printf("Warmup complete for Kogge_Stone_scan_kernel_shfl_up_sync_version_coarsening_degree_12\n");
+
+        // 2.2 Benchmarking
+        cudaEventRecord(start);
+        for (int i=0;i<BENCHMARK_REPEAT;i++){
+            Kogge_Stone_scan_kernel_shfl_up_sync_version_coarsening_degree_12<<<grid, block>>>(d_input, d_output, TOTAL_N);
+        }
+        cudaEventRecord(stop);
+        cudaEventSynchronize(stop);
+
+        // 防止静默出错（但这一部分似乎也是不必要的）
+        err = cudaGetLastError();
+        if (err != cudaSuccess) {
+            printf("!! Kernel Launch Failed !! Reason: %s\n", cudaGetErrorString(err));
+        }
+
+        cudaEventElapsedTime(&milliseconds, start, stop);
+        printf("Execution time for Kogge_Stone_scan_kernel_shfl_up_sync_version_coarsening_degree_12: %f ms\n\n\n",milliseconds/BENCHMARK_REPEAT);
+        gb_per_sec = total_bytes / ((milliseconds / BENCHMARK_REPEAT) / 1000.0) / 1e9;
+        printf("Effective Bandwidth: %.2f GB/s\n", gb_per_sec);
+        printf("------------------------------------------------------------------------------------------------------------\n");
+
+
+        //////////////////////////////////////////////////////////////////////////////////////
+        //     4. Kogge_Stone_scan_kernel_shfl_up_sync_version_coarsening_degree_16
+        //////////////////////////////////////////////////////////////////////////////////////
+        items = BLOCK_SIZE*16;
+        grid_size = (TOTAL_N+items-1)/items;
+
+        block.x = BLOCK_SIZE;
+        grid.x = grid_size;
+        printf("grid:  %5d, %5d, %5d\n", grid.x, grid.y, grid.z);
+        printf("block: %5d, %5d, %5d\n", block.x, block.y, block.z);
+
+        // 2.1 Warmup
+        for (int i=0;i<10;i++){
+            Kogge_Stone_scan_kernel_shfl_up_sync_version_coarsening_degree_16<<<grid, block>>>(d_input, d_output, TOTAL_N);
+        }
+        cudaDeviceSynchronize();
+        printf("Warmup complete for Kogge_Stone_scan_kernel_shfl_up_sync_version_coarsening_degree_16\n");
+
+        // 2.2 Benchmarking
+        cudaEventRecord(start);
+        for (int i=0;i<BENCHMARK_REPEAT;i++){
+            Kogge_Stone_scan_kernel_shfl_up_sync_version_coarsening_degree_16<<<grid, block>>>(d_input, d_output, TOTAL_N);
+        }
+        cudaEventRecord(stop);
+        cudaEventSynchronize(stop);
+
+        // 防止静默出错（但这一部分似乎也是不必要的）
+        err = cudaGetLastError();
+        if (err != cudaSuccess) {
+            printf("!! Kernel Launch Failed !! Reason: %s\n", cudaGetErrorString(err));
+        }
+
+        cudaEventElapsedTime(&milliseconds, start, stop);
+        printf("Execution time for Kogge_Stone_scan_kernel_shfl_up_sync_version_coarsening_degree_16: %f ms\n\n\n",milliseconds/BENCHMARK_REPEAT);
+        gb_per_sec = total_bytes / ((milliseconds / BENCHMARK_REPEAT) / 1000.0) / 1e9;
+        printf("Effective Bandwidth: %.2f GB/s\n", gb_per_sec);
+        printf("------------------------------------------------------------------------------------------------------------\n");
+
+
+        //////////////////////////////////////////////////////////////////////////////////////
+        //     5. Kogge_Stone_scan_kernel_shfl_up_sync_version_coarsening_degree_20
+        //////////////////////////////////////////////////////////////////////////////////////
+        items = BLOCK_SIZE*20;
+        grid_size = (TOTAL_N+items-1)/items;
+
+        block.x = BLOCK_SIZE;
+        grid.x = grid_size;
+        printf("grid:  %5d, %5d, %5d\n", grid.x, grid.y, grid.z);
+        printf("block: %5d, %5d, %5d\n", block.x, block.y, block.z);
+
+        // 2.1 Warmup
+        for (int i=0;i<10;i++){
+            Kogge_Stone_scan_kernel_shfl_up_sync_version_coarsening_degree_20<<<grid, block>>>(d_input, d_output, TOTAL_N);
+        }
+        cudaDeviceSynchronize();
+        printf("Warmup complete for Kogge_Stone_scan_kernel_shfl_up_sync_version_coarsening_degree_20\n");
+
+        // 2.2 Benchmarking
+        cudaEventRecord(start);
+        for (int i=0;i<BENCHMARK_REPEAT;i++){
+            Kogge_Stone_scan_kernel_shfl_up_sync_version_coarsening_degree_20<<<grid, block>>>(d_input, d_output, TOTAL_N);
+        }
+        cudaEventRecord(stop);
+        cudaEventSynchronize(stop);
+
+        // 防止静默出错（但这一部分似乎也是不必要的）
+        err = cudaGetLastError();
+        if (err != cudaSuccess) {
+            printf("!! Kernel Launch Failed !! Reason: %s\n", cudaGetErrorString(err));
+        }
+
+        cudaEventElapsedTime(&milliseconds, start, stop);
+        printf("Execution time for Kogge_Stone_scan_kernel_shfl_up_sync_version_coarsening_degree_20: %f ms\n\n\n",milliseconds/BENCHMARK_REPEAT);
+        gb_per_sec = total_bytes / ((milliseconds / BENCHMARK_REPEAT) / 1000.0) / 1e9;
+        printf("Effective Bandwidth: %.2f GB/s\n", gb_per_sec);
+        printf("------------------------------------------------------------------------------------------------------------\n");
+    }
+
+
+    auto now_end = std::chrono::system_clock::now();
+    std::time_t end_time = std::chrono::system_clock::to_time_t(now_end);
+    std::cout << "Benchmark Finished at: " 
+              << std::put_time(std::localtime(&end_time), "%Y-%m-%d %H:%M:%S") 
+              << std::endl;
+
+
+    cudaEventDestroy(start);
+    cudaEventDestroy(stop);
+
+    cudaFree(d_input);
+    cudaFree(d_output);
+    return 0;
+}
